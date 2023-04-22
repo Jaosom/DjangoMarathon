312abf0e7be0dde1556b1a536f75f0e9d80f5ed1
Jaosom <92562301+Jaosom@users.noreply.github.com>
2023-04-22 17:12:18 +0700

    เพิ่ม model แล้ว

--- mywebsite/myapp/models.py
+++ mywebsite/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# ใช้กำหนด ฐานข้อมูล
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=10)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.name 
